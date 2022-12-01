3ec27942f2087cd65afc1051bdd3ee6a4885f219
Nebiyu <estifbj@gmail.com>
2022-11-30 18:59:10 -0600

    Update complaints_script.R

--- complaints_script.R
+++ complaints_script.R
@@ -4,9 +4,14 @@ library(readxl)
 library(lubridate)
 library(explore)
 library(ggplot2)
+library(SentimentAnalysis)
 rm(list = ls())
 
 #setting working directory
 setwd("C:/Users/estif/OneDrive/Documents/Data-331/Final Project/data")
 
 df_complaints <- read_csv("Consumer_Complaints.csv")
+
+complaints = df_complaints$`Consumer complaint narrative`
+
+sentiment = analyzeSentiment(complaints)
