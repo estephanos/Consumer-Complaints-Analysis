5197b3ccbf2e3195088703843be5172cfdf535ee
Chloef33 <104874388+Chloef33@users.noreply.github.com>
2022-12-06 19:46:29 -0600

    Update complaints_script.R

--- complaints_script.R
+++ complaints_script.R
@@ -12,6 +12,15 @@ setwd("C:/Users/estif/OneDrive/Documents/Data-331/Final Project/data")
 
 df_complaints <- read_csv("Consumer_Complaints.csv")
 
+complaints$ZIP.code <- as.numeric(complaints$ZIP.code)
+complaints$Consumer.consent.provided. <- ifelse(complaints$Consumer.consent.provided. == "", "N/A",complaints$Consumer.consent.provided.)
+complaints$Sub.product <- ifelse(complaints$Sub.product == "", "N/A",complaints$Sub.product)
+complaints$Sub.issue<- ifelse(complaints$Sub.issue == "", "N/A",complaints$Sub.issue)
+complaints$Consumer.complaint.narrative <- ifelse(complaints$Consumer.complaint.narrative == "", "N/A",complaints$Consumer.complaint.narrative)
+complaints$Company.public.response <- ifelse(complaints$Company.public.response == "", "N/A",complaints$Company.public.response)
+complaints$Tags<- ifelse(complaints$Tags == "", "N/A",complaints$Tags)
+complaints$Consumer.disputed.<- ifelse(complaints$Consumer.disputed. == "", "N/A",complaints$Consumer.disputed.)
+
 complaints = df_complaints$`Consumer complaint narrative`
 
 sentiment = analyzeSentiment(complaints)
